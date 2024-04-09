a598460c48de9a1581211ca9d259ebc74215ec89
adrianzambrano94 <adrian9414@gmail.com>
2024-04-08 20:31:21 -0600

    Update 20240408-20:30

---
+++ scripts/index.js
@@ -0,0 +1,19 @@
+// console.log('correcto');
+
+const xhttp = new XMLHttpRequest();
+
+xhttp.open('GET', 'json/datos.json', true);
+
+xhttp.send();
+
+xhttp.onreadystatechange = function(){
+  if(this.readyState == 4 && this.status==200){
+    console.log(this.responseText);
+    let datos = JSON.parse(this.responseText);
+    console.log(datos);
+    for (var i = 0; i < datos.length; i++){
+          document.getElementById("librosEnLista").innerHTML +=
+          '<a href="' + datos[i].href + '"><img src="' + datos[i].imgsrc + '" alt="' + datos[i].alt + '" style="height:400px;"></a>';
+        }
+  }
+}
